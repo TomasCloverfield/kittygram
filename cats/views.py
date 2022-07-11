398802fcc2b2c0ed9614afd36ec4aedc483752d3
TomasCloverfield <chefanovs@gmail.com>
2022-07-11 20:00:27 +0300

    Use generic views

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,8 @@
-from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+# from rest_framework.views import APIView
+# from rest_framework.decorators import api_view
+from rest_framework.generics import \
+    ListCreateAPIView,\
+    RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -29,15 +32,26 @@ from .serializers import CatSerializer
 #     return Response(serializer.data)
 
 
-class APICat(APIView):
-    def get(self, request):
-        cats = Cat.objects.all()
-        serializer = CatSerializer(cats, many=True)
-        return Response(serializer.data)
+# class APICat(APIView):
+#     def get(self, request):
+#         cats = Cat.objects.all()
+#         serializer = CatSerializer(cats, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request):
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+class CatList(ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
