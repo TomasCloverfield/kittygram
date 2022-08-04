e5eec71af1b5c0b8dd675fbb5efc2c66caff01df
root <chefanovs@gmail.com>
2022-08-04 21:24:02 +0000

    Comment standard authorization and use JWT.

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -40,7 +40,12 @@ urlpatterns = [
    # Все зарегистрированные в router пути доступны в router.urls
    # Включим их в головной urls.py
    path('', include(router.urls)),
-   path('api-token-auth/', views.obtain_auth_token),
+   # path('api-token-auth/', views.obtain_auth_token),
+   # Djoser создаст набор необходимых эндпоинтов.
+   # базовые, для управления пользователями в Django:
+   path('auth/', include('djoser.urls')),
+   # JWT-эндпоинты, для управления JWT-токенами:
+   path('auth/', include('djoser.urls.jwt')),
 ]
 
 
